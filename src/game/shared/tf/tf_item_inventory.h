6d9a745cbb6bcaf138b50610579ce5b503dd94ba
BetaM <foreverbetam@gmail.com>
2025-02-21 20:12:52 +0100

    Add schema tag to place custom items in loadouts

--- src/game/shared/tf/tf_item_inventory.h
+++ src/game/shared/tf/tf_item_inventory.h
@@ -215,11 +215,14 @@ public:
 
 	int					GetBaseItemCount( )			{ return m_pBaseLoadoutItems.Count(); }
 	CEconItemView*		GetBaseItem( int iIndex )	{ return m_pBaseLoadoutItems[iIndex]; }
+	int					GetSoloItemCount()			{ return m_pSoloLoadoutItems.Count(); }
+	CEconItemView*		GetSoloItem(int iIndex)		{ return m_pSoloLoadoutItems[iIndex]; }
 
 private:
 	// Base items, returned for slots that the player doesn't have anything in
 	CEconItemView				*m_pDefaultItem;
 	CUtlVector<CEconItemView*>	m_pBaseLoadoutItems;
+	CUtlVector<CEconItemView*>	m_pSoloLoadoutItems;
 
 #ifdef CLIENT_DLL
 	// On the client, we have a single inventory for the local player. Stored here, instead of in the
