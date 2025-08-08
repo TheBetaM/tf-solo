2183ec90e3d79b2ff0769440d9b534fc15f2413f
BetaM <foreverbetam@gmail.com>
2025-08-09 00:04:37 +0200

    Implement https://github.com/ValveSoftware/source-sdk-2013/pull/1483

--- src/game/client/cdll_client_int.cpp
+++ src/game/client/cdll_client_int.cpp
@@ -1713,6 +1713,17 @@ void CHLClient::ResetStringTablePointers()
 #endif
 }
 
+static void ClearClientDynamicModelList()
+{
+	// engine bugfix: clear out client dynamic model list for listen servers
+	// TODO remove this when the engine is updated
+	struct CModelInfo : IVModelInfoClient
+	{
+		CUtlVector< model_t* > m_vecDynamicModels;
+	};
+	static_cast<CModelInfo*>(modelinfo)->m_vecDynamicModels.Purge();
+}
+
 //-----------------------------------------------------------------------------
 // Purpose: Per level de-init
 //-----------------------------------------------------------------------------
@@ -1763,6 +1774,8 @@ void CHLClient::LevelShutdown( void )
 	internalCenterPrint->Clear();
 
 	messagechars->Clear();
+
+	ClearClientDynamicModelList();
 
 #ifndef TF_CLIENT_DLL
 	// don't want to do this for TF2 because we have particle systems in our
